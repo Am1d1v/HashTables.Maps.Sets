6ca114cf78fa814dd7bd6261d5da4aa70ea89342
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2024-03-14 21:18:51 +0300

    Add files via upload

--- sets.js
+++ sets.js
@@ -14,3 +14,26 @@ console.log(nameSet);
 
 // Check for values
 console.log(nameSet.has('Anna'));
+
+// Get Size
+console.log(nameSet.size);
+
+// Get for values
+console.log(nameSet.values());
+
+
+// Convert to array
+
+/* const namesArray = [];
+
+for (const user of nameSet){
+    namesArray.push(user);
+}
+console.log(namesArray); */
+
+const namesArray = [...nameSet];
+console.log(namesArray);
+
+// Clear the set
+nameSet.clear();
+console.log(nameSet);
